b1bd866496b7c3bb32c6f769718d3fd01950ce39
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-06 15:53:38 +0900

    id修正

--- database/seeders/ValuesBranchWorkStyleTypeSeeder.php
+++ database/seeders/ValuesBranchWorkStyleTypeSeeder.php
@@ -13,34 +13,42 @@ class ValuesBranchWorkStyleTypeSeeder extends Seeder
     public function run(): void
     {
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '1', 
             'name' => '完全週休2日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '2', 
             'name' => '隔週2日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '3', 
             'name' => '月1回　週休2日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '4', 
             'name' => '月2回　週休2日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '5', 
             'name' => '月3回　週休2日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '6', 
             'name' => '1年変形休日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '7', 
             'name' => '1ヵ月変形休日制', 
         ]);
 
         $values_branch_work_style_type = Values_branch_work_style_type::create([
+            'id' => '99', 
             'name' => '変形労働制', 
         ]);
     }
