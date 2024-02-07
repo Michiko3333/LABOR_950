e23356301ca163ed2804140c53f17488ad1eaa09
linjinxiu3276 <156409496+linjinxiu3276@users.noreply.github.com>
2024-02-07 15:43:44 +0900

    Feature/labor 77-fix 都道府県seeder修正 (#40)
    * 都道府県seeder修正
    
    * 配列修正
    
    * インデント修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ValuesCalendarEventCategoryTypeSeeder::class);
+        $this->call([
+            ValuesCalendarEventCategoryTypeSeeder::class，
+            PrefectureSeeder::class，
+        ]);    
     }
 }
--- database/seeders/PrefectureSeeder.php
+++ database/seeders/PrefectureSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Prefecture;
+use Illuminate\Support\Facades\DB;
 
 class PrefectureSeeder extends Seeder
 {
@@ -12,240 +12,197 @@ class PrefectureSeeder extends Seeder
      */
     public function run(): void
     {
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '01',
-            'name' => '北海道',
+        DB::table('m_prefecture')->insert([
+            [
+                'prefecture_code'   => '01',
+                'name'  => '北海道'
+            ],
+            [
+                'prefecture_code'   => '02',
+                'name'  => '青森県'
+            ],
+            [
+                'prefecture_code'   => '03',
+                'name'  => '岩手県'
+            ],
+            [
+                'prefecture_code'   => '04',
+                'name'  => '宮城県'
+            ],
+            [
+                'prefecture_code'   => '05',
+                'name'  => '秋田県'
+            ],
+            [
+                'prefecture_code'   => '06',
+                'name'  => '山形県'
+            ],
+            [
+                'prefecture_code'   => '07',
+                'name'  => '福島県'
+            ],
+            [
+                'prefecture_code'   => '08',
+                'name'  => '茨城県'
+            ],
+            [
+                'prefecture_code'   => '09',
+                'name'  => '栃木県'
+            ],
+            [
+                'prefecture_code'   => '10',
+                'name'  => '群馬県'
+            ],
+            [
+                'prefecture_code'   => '11',
+                'name'  => '埼玉県'
+            ],
+            [
+                'prefecture_code'   => '12',
+                'name'  => '千葉県'
+            ],
+            [
+                'prefecture_code'   => '13',
+                'name'  => '東京都'
+            ],
+            [
+                'prefecture_code'   => '14',
+                'name'  => '神奈川県'
+            ],
+            [
+                'prefecture_code'   => '15',
+                'name'  => '新潟県'
+            ],
+            [
+                'prefecture_code'   => '16',
+                'name'  => '富山県'
+            ],
+            [
+                'prefecture_code'   => '17',
+                'name'  => '石川県'
+            ],
+            [
+                'prefecture_code'   => '18',
+                'name'  => '福井県'
+            ],
+            [
+                'prefecture_code'   => '19',
+                'name'  => '山梨県'
+            ],
+            [
+                'prefecture_code'   => '20',
+                'name'  => '長野県'
+            ],
+            [
+                'prefecture_code'   => '21',
+                'name'  => '岐阜県'
+            ],
+            [
+                'prefecture_code'   => '22',
+                'name'  => '静岡県'
+            ],
+            [
+                'prefecture_code'   => '23',
+                'name'  => '愛知県'
+            ],
+            [
+                'prefecture_code'   => '24',
+                'name'  => '三重県'
+            ],
+            [
+                'prefecture_code'   => '25',
+                'name'  => '滋賀県'
+            ],
+            [
+                'prefecture_code'   => '26',
+                'name'  => '京都府'
+            ],
+            [
+                'prefecture_code'   => '27',
+                'name'  => '大阪府'
+            ],
+            [
+                'prefecture_code'   => '28',
+                'name'  => '兵庫県'
+            ],
+            [
+                'prefecture_code'   => '29',
+                'name'  => '奈良県'
+            ],
+            [
+                'prefecture_code'   => '30',
+                'name'  => '和歌山県'
+            ],
+            [
+                'prefecture_code'   => '31',
+                'name'  => '鳥取県'
+            ],
+            [
+                'prefecture_code'   => '32',
+                'name'  => '島根県'
+            ],
+            [
+                'prefecture_code'   => '33',
+                'name'  => '岡山県'
+            ],
+            [
+                'prefecture_code'   => '34',
+                'name'  => '広島県'
+            ],
+            [
+                'prefecture_code'   => '35',
+                'name'  => '山口県'
+            ],
+            [
+                'prefecture_code'   => '36',
+                'name'  => '徳島県'
+            ],
+            [
+                'prefecture_code'   => '37',
+                'name'  => '香川県'
+            ],
+            [
+                'prefecture_code'   => '38',
+                'name'  => '愛媛県'
+            ],
+            [
+                'prefecture_code'   => '39',
+                'name'  => '高知県'
+            ],
+            [
+                'prefecture_code'   => '40',
+                'name'  => '福岡県'
+            ],
+            [
+                'prefecture_code'   => '41',
+                'name'  => '佐賀県'
+            ],
+            [
+                'prefecture_code'   => '42',
+                'name'  => '長崎県'
+            ],
+            [
+                'prefecture_code'   => '43',
+                'name'  => '熊本県'
+            ],
+            [
+                'prefecture_code'   => '44',
+                'name'  => '大分県'
+            ],
+            [
+                'prefecture_code'   => '45',
+                'name'  => '宮崎県'
+            ],
+            [
+                'prefecture_code'   => '46',
+                'name'  => '鹿児島県'
+            ],
+            [
+                'prefecture_code'   => '47',
+                'name'  => '沖縄県'
+            ],
+                
+    
         ]);
-
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '02',
-            'name' => '青森県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '03',
-            'name' => '岩手県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '04',
-            'name' => '宮城県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '05',
-            'name' => '秋田県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '06',
-            'name' => '山形県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '07',
-            'name' => '福島県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '08',
-            'name' => '茨城県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '09',
-            'name' => '栃木県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '10',
-            'name' => '群馬県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '11',
-            'name' => '埼玉県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '12',
-            'name' => '千葉県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '13',
-            'name' => '東京都',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '14',
-            'name' => '神奈川県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '15',
-            'name' => '新潟県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '16',
-            'name' => '富山県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '17',
-            'name' => '石川県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '18',
-            'name' => '福井県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '19',
-            'name' => '山梨県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '20',
-            'name' => '長野県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '21',
-            'name' => '岐阜県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '22',
-            'name' => '静岡県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '23',
-            'name' => '愛知県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '24',
-            'name' => '三重県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '25',
-            'name' => '滋賀県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '26',
-            'name' => '京都府',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '27',
-            'name' => '大阪府',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '28',
-            'name' => '兵庫県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '29',
-            'name' => '奈良県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '30',
-            'name' => '和歌山県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '31',
-            'name' => '鳥取県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '32',
-            'name' => '島根県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '33',
-            'name' => '岡山県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '34',
-            'name' => '広島県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '35',
-            'name' => '山口県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '36',
-            'name' => '徳島県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '37',
-            'name' => '香川県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '38',
-            'name' => '愛媛県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '39',
-            'name' => '高知県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '40',
-            'name' => '福岡県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '41',
-            'name' => '佐賀県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '42',
-            'name' => '長崎県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '43',
-            'name' => '熊本県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '44',
-            'name' => '大分県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '45',
-            'name' => '宮崎県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '46',
-            'name' => '鹿児島県',
-        ]);
-        
-        $prefecture = Prefecture::create([
-            'prefecture_code' => '47',
-            'name' => '沖縄県',
-        ]);
-        
     }
 }
