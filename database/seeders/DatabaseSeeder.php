47b2dcff248e537be0a6466498e85945cb8b1806
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-07 15:57:04 +0900

    書き方修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ValuesCalendarEventCategoryTypeSeeder::class，
+            ValuesEmployeeOccupationTypeSeeder::class，
             PrefectureSeeder::class，
         ]);    
     }
--- database/seeders/ValuesEmployeeOccupationTypeSeeder.php
+++ database/seeders/ValuesEmployeeOccupationTypeSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Values_employee_occupation_type;
+use Illuminate\Support\Facades\DB;
 
 class ValuesEmployeeOccupationTypeSeeder extends Seeder
 {
@@ -12,59 +12,51 @@ class ValuesEmployeeOccupationTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '01', 
-            'name' => '管理的職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '02', 
-            'name' => '専門的・技術的職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '03', 
-            'name' => '事務的職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '04', 
-            'name' => '販売の職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '05', 
-            'name' => 'サービスの職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '06', 
-            'name' => '保安の職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '07', 
-            'name' => '農林漁業の職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '08', 
-            'name' => '生産工程の職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '09', 
-            'name' => '輸送・機械運転の職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '10', 
-            'name' => '建設・採掘の職業', 
-        ]);
-
-        $values_employee_occupation_type = Values_employee_occupation_type::create([
-            'option_no' => '11', 
-            'name' => '運搬・清掃・包装等の職業', 
+        DB::table('m_values_employee_occupation_type')->insert([
+            [
+                'option_no' => '01', 
+                'name' => '管理的職業',
+            ],
+            [
+                'option_no' => '02', 
+                'name' => '専門的・技術的職業', 
+            ],
+            [
+                'option_no' => '03', 
+                'name' => '事務的職業',     
+            ],
+            [
+                'option_no' => '04', 
+                'name' => '販売の職業', 
+            ],
+            [
+                'option_no' => '05', 
+                'name' => 'サービスの職業',
+            ],
+            [
+                'option_no' => '06', 
+                'name' => '保安の職業', 
+            ],
+            [
+                'option_no' => '07', 
+                'name' => '農林漁業の職業', 
+            ],
+            [
+                'option_no' => '08', 
+                'name' => '生産工程の職業', 
+            ],
+            [
+                'option_no' => '09', 
+                'name' => '輸送・機械運転の職業', 
+            ],
+            [
+                'option_no' => '10', 
+                'name' => '建設・採掘の職業', 
+            ],
+            [
+                'option_no' => '11', 
+                'name' => '運搬・清掃・包装等の職業', 
+            ],
         ]);
     }
 }
