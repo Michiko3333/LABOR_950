467da71938587479e1b596f1cb71334a2af01267
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-07 16:34:57 +0900

    書き方修正

--- database/seeders/ValuesEmployeeEmployeeTypeSeeder.php
+++ database/seeders/ValuesEmployeeEmployeeTypeSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Values_employee_employee_type;
+use Illuminate\Support\Facades\DB;
 
 class ValuesEmployeeEmployeeTypeSeeder extends Seeder
 {
@@ -12,34 +12,31 @@ class ValuesEmployeeEmployeeTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $values_employee_employee_type = Values_employee_employee_type::create([
-            'id' => '1', 
-            'name' => '代表取締役', 
-        ]);
-
-        $values_employee_employee_type = Values_employee_employee_type::create([
-            'id' => '2', 
-            'name' => '取締役', 
-        ]);
-
-        $values_employee_employee_type = Values_employee_employee_type::create([
-            'id' => '3', 
-            'name' => '執行役員', 
-        ]);
-
-        $values_employee_employee_type = Values_employee_employee_type::create([
-            'id' => '4', 
-            'name' => '外部役員', 
-        ]);
-
-        $values_employee_employee_type = Values_employee_employee_type::create([
-            'id' => '5', 
-            'name' => '顧問', 
-        ]);
-
-        $values_employee_employee_type = Values_employee_employee_type::create([
-            'id' => '9', 
-            'name' => '一般社員', 
+        DB::table('m_values_employee_employee_type')->insert([
+            [
+                'id' => '1', 
+                'name'=> '代表取締役',
+            ],
+            [
+                'id' => '2', 
+                'name'=> '取締役', 
+            ],
+            [
+                'id' => '3', 
+                'name'=> '執行役員', 
+            ],
+            [
+                'id' => '4', 
+                'name'=> '外部役員', 
+            ],
+            [
+                'id' => '5', 
+                'name'=> '顧問', 
+            ],
+            [
+                'id' => '9', 
+                'name'=> '一般社員', 
+            ],
         ]);
     }
 }
