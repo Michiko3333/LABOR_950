95296024bbfcdff02419725d9ee08f5284e43438
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-07 16:54:33 +0900

    書き方修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ValuesCalendarEventCategoryTypeSeeder::class，
+            ValuesBranchWorkStyleTypeSeeder::class，
             PrefectureSeeder::class，
         ]);    
     }
