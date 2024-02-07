081cd4f173dbe022094dc07eda94f6b792af801b
linjinxiu3276 <156409496+linjinxiu3276@users.noreply.github.com>
2024-02-07 12:30:38 +0900

    設定値マスタ系のモデル (#37)

---
+++ app/Models/Values_employee_employment_route.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Values_employee_employment_route extends Model
+{
+    protected $table = 'm_values_employee_employment_route';
+    protected $primaryKey = 'id';
+}
