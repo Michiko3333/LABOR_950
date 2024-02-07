3eea02b9f98fc946b11ccc0c6260e35b52508172
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-07 16:09:04 +0900

    書き方修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ValuesCalendarEventCategoryTypeSeeder::class，
+            ValuesEmployeeEmploymentStatusSeeder::class，
             PrefectureSeeder::class，
         ]);    
     }
--- database/seeders/ValuesEmployeeEmploymentStatusSeeder.php
+++ database/seeders/ValuesEmployeeEmploymentStatusSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Values_employee_employment_status;
+use Illuminate\Support\Facades\DB;
 
 class ValuesEmployeeEmploymentStatusSeeder extends Seeder
 {
@@ -12,33 +12,14 @@ class ValuesEmployeeEmploymentStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => '日雇', 
-        ]);
-
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => '派遣', 
-        ]);
-
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => 'パートタイム', 
-        ]);
-
-
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => '有期契約労働者', 
-        ]);
-
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => '季節的雇用', 
-        ]);
-
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => '船員', 
-        ]);
-
-        $values_employee_employment_status = Values_employee_employment_status::create([
-            'name' => 'その他', 
+        DB::table('m_values_employee_employment_status')->insert([
+            ['name' => '日雇'],
+            ['name' => '派遣'],
+            ['name' => 'パートタイム'],
+            ['name' => '有期契約労働者'],
+            ['name' => '季節的雇用'],
+            ['name' => '船員'],
+            ['name' => 'その他']
         ]);
     }
 }
