ec17d4f3012f85d7088d59eab5ba8a39a2b814c3
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-07 16:11:51 +0900

    書き方修正

--- database/seeders/ValuesEmployeeEmploymentTypeSeeder.php
+++ database/seeders/ValuesEmployeeEmploymentTypeSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Values_employee_employment_type;
+use Illuminate\Support\Facades\DB;
 
 class ValuesEmployeeEmploymentTypeSeeder extends Seeder
 {
@@ -12,12 +12,9 @@ class ValuesEmployeeEmploymentTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $values_employee_employment_type = Values_employee_employment_type::create([
-            'name' => '派遣・請負労働者として主として当該事業所以外で就労する場合', 
-        ]);
-
-        $values_employee_employment_type = Values_employee_employment_type::create([
-            'name' => '1に該当しない場合', 
+        DB::table('m_values_employee_employment_type')->insert([
+            ['name' => '派遣・請負労働者として主として当該事業所以外で就労する場合'],
+            ['name' => '1に該当しない場合']
         ]);
     }
 }
