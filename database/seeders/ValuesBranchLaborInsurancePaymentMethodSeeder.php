c8b624ab796fe1ce808a66b60c86f0188ec59da8
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-14 17:59:00 +0900

    修正

--- database/seeders/ValuesBranchLaborInsurancePaymentMethodSeeder.php
+++ database/seeders/ValuesBranchLaborInsurancePaymentMethodSeeder.php
@@ -14,7 +14,7 @@ class ValuesBranchLaborInsurancePaymentMethodSeeder extends Seeder
     {
         DB::table('m_values_branch_labor_insurance_payment_method')->insert([
             ['name' => '口座振替'],
-            ['name' => '窓口納付   （窓口の場合納付期限が異なる。直接納付は2週間も納付が早い）']
+            ['name' => '窓口納付']
         ]);
     }
 }
