af65c9a128ad6ef96f2f08b628a6b69371650a7d
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-19 14:30:11 +0900

    被扶養者の被扶養者でなくなった理由区分設定値マスタSeeder

---
+++ database/seeders/ValuesDependentDependentRemoveReasonTypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ValuesDependentDependentRemoveReasonTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('m_values_dependent_dependent_remove_reason_type')->insert([
+            ['name' => '死亡'],
+            ['name' => '離婚'],
+            ['name' => '就職・収入増加'],
+            ['name' => '75歳到達'],
+            ['name' => '障害認定'],
+            ['name' => 'その他']
+        ]);
+    }
+}
