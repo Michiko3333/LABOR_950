5c2d33292874997573f7d04c288ce148cadfdc2e
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-19 14:17:15 +0900

    被扶養者の電話番号区分設定値マスタSeeder

---
+++ database/seeders/ValuesDependentTelTypeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ValuesDependentTelTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('m_values_dependent_tel_type')->insert([
+            ['name' => '自宅'],
+            ['name' => '携帯'],
+            ['name' => '勤務先'],
+            ['name' => 'その他']
+        ]);
+
+    }
+}
