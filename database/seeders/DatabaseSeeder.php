8f52301844edaec9a87b6201df5d7d8d841442fc
linjinxiu3276 <lin.jinxiu@korat.co.jp>
2024-02-28 16:23:19 +0900

    労災保険適用事業所マスタSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -61,7 +61,8 @@ class DatabaseSeeder extends Seeder
         $this->call(LaborSupervisionSeeder::class);
         $this->call(LaborBureauSeeder::class);
         $this->call(HelloWorkSeeder::class);
-        // $this->call(ValuesEmployeeInsuredTypeSeeder::class);
+        $this->call(ValuesEmployeeInsuredTypeSeeder::class);
         $this->call(ValuesEmployeeChildcareReacquisitionReasonSeeder::class);
+        $this->call(CompensationApplicableIndustrySeeder::class);
     }
 }
