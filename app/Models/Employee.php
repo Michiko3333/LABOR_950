4155daf4c50474ece7121a6d4371f14408467f66
linjinxiu3276 <156409496+linjinxiu3276@users.noreply.github.com>
2024-03-01 14:27:11 +0900

    モデル関係修正 (#101)

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -11,7 +11,7 @@ class Employee extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class,'employee_id','id');
     }
 
     public function branch()
