1569c0711048d9510bb82440438a731d3ee84509
dounoue-korat <151009701+dounoue-korat@users.noreply.github.com>
2024-04-25 17:52:29 +0900

    添付書類ラジオボタンxmlに反映 (#218)
    * 添付書類ラジオボタンxmlに反映
    
    * 修正

--- app/Http/Controllers/Ledger/HealthInsuranceDependentChangeController.php
+++ app/Http/Controllers/Ledger/HealthInsuranceDependentChangeController.php
@@ -105,7 +105,7 @@ class HealthInsuranceDependentChangeController extends Controller
             $request->merge(['attachment' => $attachment]);
         }
 
-        $radio_keys = ["radio_file_insurance","radio_ffile_dependent","radio_file_tax_exempt","radio_file_currently_enrolled","radio_file_basic_pension","radio_file_livelihood_maintenance","radio_file_business_owner","radio_file_medical_insurer", "radio_file_other"];
+        $radio_keys = ["radio_file_insurance","radio_file_dependent","radio_file_tax_exempt","radio_file_currently_enrolled","radio_file_basic_pension","radio_file_livelihood_maintenance","radio_file_business_owner","radio_file_medical_insurer", "radio_file_other"];
 
         foreach ($radio_keys as $key) {
             if (!$request->has($key)) {
