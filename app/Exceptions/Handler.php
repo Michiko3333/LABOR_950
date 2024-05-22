ee465a448d5f8a1b953252120e751bd320324164
koratmatsuda <121213053+koratmatsuda@users.noreply.github.com>
2024-05-22 20:43:20 +0900

    共通エラー画面追加 (#314)
    Co-authored-by: Tetsuya Matsuda <tetsuya@fe8works.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +27,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // ここで特定のエラーコードに対してビューを指定します。
+        if ($this->isHttpException($exception)) {
+            return response()->view('errors.common', ['exception' => $exception]);
+        }
+
+        return parent::render($request, $exception);
     }
 }
