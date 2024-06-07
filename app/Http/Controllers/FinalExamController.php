7bd289ae844f55a169e46cff31629f51c73b3fe4
yoshiya-koga <116784582+yoshiya-koga@users.noreply.github.com>
2024-06-07 12:34:01 +0900

    Feature/labor 416 2 (#367)
    * 公文書データ取得前検査の完了
    
    * 第二次最終試験用データ作成・調整
    
    * 不要削除
    
    * 本番用調整
    
    * 本番用調整
    
    * 本番用調整
    
    * コード戻りの修正

--- app/Http/Controllers/FinalExamController.php
+++ app/Http/Controllers/FinalExamController.php
@@ -135,7 +135,7 @@ class FinalExamController extends Controller
     // 申請案件一覧取得　artisanコマンド
     public static function getlist_command($companyId, $examNo)
     {
-        $date_from = '2024-05-16';
+        $date_from = '2024-05-30';
         $today = new \DateTime();
         $date_to = $today->format('Y-m-d');
 
@@ -297,7 +297,7 @@ class FinalExamController extends Controller
     public static function get_notification_list_command($companyId, $examNo)
     {
         $title = '申請案件に関する通知一覧取得';
-        $date_from = '2024-05-16';
+        $date_from = '2024-05-30';
         $today = new \DateTime();
         $date_to = $today->format('Y-m-d');
         $account = Egov_account::where('company_id', $companyId)->where('delete_flg', 0)->first();
