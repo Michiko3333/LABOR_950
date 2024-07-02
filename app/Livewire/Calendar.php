b272bc79c88053d99baa0da43c38c938da352913
koratmatsuda <121213053+koratmatsuda@users.noreply.github.com>
2024-07-02 16:44:39 +0900

    fix cat valid (#438)
    Co-authored-by: Tetsuya Matsuda <tetsuya@fe8works.com>

--- app/Livewire/Calendar.php
+++ app/Livewire/Calendar.php
@@ -451,6 +451,9 @@ class Calendar extends Component
         $max = strtotime($this->years['one_year_later'] . '-12-31 23:59');
 
         if (mb_strlen($data['inputs_name']) > 20) return false;
+        if ($data['inputs_category'] < 1 || $data['inputs_category'] > count($this->category_types)) {
+            return;
+        }
 
         if ($min > $data['from'] || $max < $data['from']) return false;
 
